efa60a9a08a972802ab89692a752128466b07399
rubensrvsc <Rubensspfc100@gmail.com>
2021-01-16 22:02:59 -0300

    refactor(denuntiation): melhorando a mensagem dos enums

--- DenunciaApi/app/models/denunciation.rb
+++ DenunciaApi/app/models/denunciation.rb
@@ -7,8 +7,8 @@ class Denunciation < ApplicationRecord
     enum status: {lida: 0, arquivada: 1, deletada: 2}
 
 
-    enum plagio: {sim_plagio: 0, nao_plagio: 1}
-    enum abuso: {sim_abuso: 0, nao_abuso: 1}
-    enum injuria: {sim_injuria: 0, nao_injuria: 1}
-    enum outro: {sim_outro: 0, nao_outro: 1}
+    enum plagio: {acusação_de_plagio: 0, sem_acusação_de_plagio: 1}
+    enum abuso: {acusação_de_abuso: 0, sem_acusação_de_abuso: 1}
+    enum injuria: {acusação_de_injuria: 0, sem_acusação_de_injuria: 1}
+    enum outro: {acusação_de_outro: 0, sem_acusação_de_outro: 1}
 end
